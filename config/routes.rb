7bbd5460abc04bf9ec73dd3a11191d97ec1a1dae
advya <50411324+Advya@users.noreply.github.com>
2021-04-15 00:01:55 +0300

    resources for comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   get 'terms' => 'terms#index'
   get 'about' => 'about#index'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
   
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
