942e4afabd737d0352d74f92afe192cc2d1cf886
advya <50411324+Advya@users.noreply.github.com>
2021-04-15 15:03:49 +0300

    comments show method on articles page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
-  
+  resource :comments, only: [:create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
