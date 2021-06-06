e8ada21721e94f17d6e4a3fe048f9a581a24070a
advya <50411324+Advya@users.noreply.github.com>
2021-06-06 20:19:04 +0300

    test for article method

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,4 +8,15 @@ describe Article do
     describe "associations" do
         it { should have_many :comments }    
     end
+
+    describe "#subject" do
+        it "returns article title" do
+            #создаем article
+            article = create(:article, :title =>'test')
+            # assert
+            expect(article.subject).to eq 'test'
+        end
+    end
+
+
 end
