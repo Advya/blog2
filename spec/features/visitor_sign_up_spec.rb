60df7abe3ed58938e7578f17561bb143d2580b25
advya <50411324+Advya@users.noreply.github.com>
2021-06-09 13:34:33 +0300

    Update visitor_sign_up_spec.rb

--- spec/features/visitor_sign_up_spec.rb
+++ spec/features/visitor_sign_up_spec.rb
@@ -1,15 +1,11 @@
 require "rails_helper"
 
+
 feature "sign up" do
     scenario "allows visitor to sign up" do
        visit new_user_registration_path
-
-       fill_in :user_email, with: "user@example.com"
-       fill_in :user_username, with: "user"
-       fill_in :user_password, with: "user@example.com"
-       fill_in :user_password_confirmation, with: "user@example.com"
-
-       click_button 'Sign up'
+       sign_up
        expect(page).to have_content I18n.t('devise.registrations.signed_up')
     end    
 end
+
